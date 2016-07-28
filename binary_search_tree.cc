5277aa6a74500b9a01d7f539d4c80d993acea179
local <kevinkim1991@gmail.com>
2016-07-28 07:08:54 -0400

    path between nodes algorithm

--- binary_search_tree.cc
+++ binary_search_tree.cc
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <climits>
+#define min(a,b) ((a) < (b) ? (a) : (b))
 using namespace std;
 
 struct node {
@@ -82,7 +83,10 @@ class BST {
       return cur->data;
     return find_predecessor(cur->right);
   }
-  
+
+  /*
+    Checks to see if Tree is BST.
+   */
   bool isBST(node *cur,int LMAX, int RMAX){
     if (cur == NULL) return true;
     if (LMAX < cur->data && cur->data <= RMAX){
@@ -90,6 +94,20 @@ class BST {
         isBST(cur->right,cur->data,RMAX);
     }
     return false;
+  }
+
+  bool find_path_to_node(vector<int> &path, node *cur, int d){
+    if (cur == NULL)
+      return false; // Node not found
+    if (cur->data == d){
+      path.push_back(cur->data);
+      return true;
+    }
+    path.push_back(cur->data);
+    if (d < cur->data)
+      return find_path_to_node(path,cur->left,d);
+    else
+      return find_path_to_node(path,cur->right,d);
   }
 public:
   BST():head(NULL){}
@@ -109,6 +127,35 @@ public:
   bool isBST(){
     return isBST(head,INT_MIN,INT_MAX);
   }
+
+  // We will find the path to s and path to e
+  // and compare. The last common node between
+  // the two paths is part of the path between s
+  // and e. Any other common nodes are not part of the
+  // path and every uncommon nodes are part of the path
+  void find_path_between_nodes(int s, int e){
+    vector<int> s_path;
+    vector<int> e_path;
+    if (!find_path_to_node(s_path,head,s)){
+      cout << "No such path to: " << s << endl;
+      return;
+    }
+    if (!find_path_to_node(e_path,head,e)){
+      cout << "No such path to: " << e << endl;
+      return;
+    }
+    // find last common
+    int i;
+    for (i = 0; i < min(s_path.size(),e_path.size())-1; ++i)
+      if (s_path[i+1] != e_path[i+1])
+        break;
+    for (int s = s_path.size()-1; s >= i; --s)
+      cout << s_path[s] << " ";
+    for (int e = i+1; e < e_path.size(); ++e)
+      cout << e_path[e] << " ";
+    cout << endl;
+  }
+
   
 };
 
@@ -125,6 +172,8 @@ void knuth_shuffle(vector<int> &L){
 }
 
 int main(){
+    // TEST1
+    /*
     BST T;
     vector<int> list;
     int input;
@@ -144,5 +193,13 @@ int main(){
     T.remove(input);
     T.print();
     cout << T.isBST() << endl;
+    */
+    // TEST2
+    BST T;
+    vector<int> list = {10,15,19,13,11,14,16,17,18,
+                        5,3,8,9,6,7,1,4};
+    for (int i = 0; i < list.size(); ++i)
+      T.insert(list[i]);
+    T.find_path_between_nodes(15,20);
     return 0;
   }
