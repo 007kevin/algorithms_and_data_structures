c9541bb40b752b73d6b48def10dfa62c10602a49
kevin <kevin>
2016-12-28 11:43:22 -0800

    euler tot

--- EulerTotient.cc
+++ EulerTotient.cc
@@ -1,5 +1,5 @@
 /*
-  Euler totient based on the mathematical foruma
+  Euler totient based on the mathematical forumla
   φ(n) = n * (1 – 1/p1) * (1 – 1/p2) * … * (1 – 1/pk)
  */
 #include <iostream>
@@ -8,10 +8,12 @@ using namespace std;
 int fi(int n){
   int r = n;
   for (int i = 2; i*i <= n; ++i){
+    cout << i << endl;
     if (n%i==0) r-=r/i;
     while (n%i==0) n/=i;
   }
   if (n>1) r-=r/n;
+  cout << n << endl;
   return r;
 }
 
